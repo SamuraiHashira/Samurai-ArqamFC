5434ab0fc25e1016ac24fc618a16ce8e89a4de79
Samurai <68142138+SamuraiHashira@users.noreply.github.com>
2023-10-25 02:28:08 +0300

    Update background.js

--- Password  Generator/background.js
+++ Password  Generator/background.js
@@ -2,23 +2,26 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("Extension Installed");
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
     if (request.length) {
-        fetch('https://github.com/SamuraiHashira/Samurai-ArqamFC/generate_password', {
-            method: 'POST',
+        const url = `https://password-generator1.p.rapidapi.com/api/generePassWd?len=${request.length}`;
+        const options = {
+            method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ length: request.length }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            sendResponse({ password: data.password });
-        })
-        .catch(error => {
-            console.error('Error:', error);
+                'X-RapidAPI-Key': '179b629e36mshf99f72aa1027ca9p162750jsnc29746bee287',
+                'X-RapidAPI-Host': 'password-generator1.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+            sendResponse({ password: result });
+        } catch (error) {
+            console.error(error);
             sendResponse({ password: 'Error generating password' });
-        });
+        }
         return true;
     }
 });
